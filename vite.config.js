7f963cd2df94ad67ae8be248f69657c80776ddf4
BHD1official <gapa.bhd1@gmail.com>
2025-02-24 16:47:56 +0200

    1

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import vue from '@vitejs/plugin-vue';
 import { VitePWA } from 'vite-plugin-pwa';
 
 export default defineConfig({
+  base: process.env.NODE_ENV === 'production' ? '/Step-To-BHD1/' : '/',
   plugins: [vue(), VitePWA({ registerType: 'autoUpdate' })],
 });
 
